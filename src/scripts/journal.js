0f36bef1b0f2783a194b38bb534a1892532f20ce
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 18:02:24 -0500

    stab at following part two instructions

---
+++ src/scripts/journal.js
@@ -0,0 +1,45 @@
+
+console.log("hello, darlin'");
+
+// Define the keys and value for a JavaScript object that
+// represents a journal entry about what you learned today
+
+const objectsJournalEntry = {
+  objects = {
+    basicInfo: "the building blocks for everything js, a method of defining details of a single thing",
+    curlyBraces: true,
+    squareBrackets: false,
+    contain: "keys and values separated by a colon, pairs are called properties",
+    dataCommaSeparated: true,
+    canBeAssignedToAVariable: true,
+    endsInSemicolon: false
+  },
+  arrays = {
+    basicInfo: "collections of similar things",
+    curlyBraces: false,
+    squareBrackets: true,
+    contain: "keys and values separated by a colon, pairs are called properties",
+    dataCommaSeparated: true,
+    canBeAssignedToAVariable: true,
+    endsInSemicolon: false
+  }
+};
+
+const notationJournalEntry = {
+  dotNotation = {
+    example: "variableContainingTheObject.keyName"
+  },
+  bracketNotation = {
+    example: 'variableContainingTheObject["keyName"]'
+  }
+};
+
+// define an appropriately named variable that will have the value of an array
+
+const entries = []
+
+// use the push() method to add the journalEntry object you defined above to the array
+
+entries.push(objectsJournalEntry);
+
+console.log(entries);
