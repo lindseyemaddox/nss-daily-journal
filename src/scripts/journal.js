e3f7003536342ebe195d90bc50daeef22581bb98
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-23 14:55:35 -0500

    did brenda-clarified step one

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -4,48 +4,35 @@ console.log("hello, darlin'");
 // Define the keys and value for a JavaScript object that
 // represents a journal entry about what you learned today
 
-const objectsJournalEntry = {
-  objects = {
-    basicInfo: "the building blocks for everything js, a method of defining details of a single thing",
-    curlyBraces: true,
-    squareBrackets: false,
-    contain: "keys and values separated by a colon, pairs are called properties",
-    dataCommaSeparated: true,
-    canBeAssignedToAVariable: true,
-    endsInSemicolon: false
-  },
-  arrays = {
-    basicInfo: "collections of similar things",
-    curlyBraces: false,
-    squareBrackets: true,
-    contain: "keys and values separated by a colon, pairs are called properties",
-    dataCommaSeparated: true,
-    canBeAssignedToAVariable: true,
-    endsInSemicolon: false
-  }
-};
+// addJournalEntry = (journalEntry) => {
+//   return `
+//     <div class="journalEntry">
+//       <h2>${journalEntry.date}</h2>
+//       <p>Concepts Covered: ${journalEntry.concepts}</p>
+//       <p>Entry: ${journalEntry.entry}</p>
+//       <p>Mood: ${journalEntry.mood}</p>
+//     </div>
+//   `
+// }
 
-const notationJournalEntry = {
-  dotNotation = {
-    example: "variableContainingTheObject.keyName"
-  },
-  bracketNotation = {
-    example: 'variableContainingTheObject["keyName"]'
-  }
-};
+myJournalEntry = {
+  title: "It'll Get Better",
+  date: "April 23",
+  entry: "Ugly cried a lot today.",
+  mood: "tearful"
+}
+
 
 // define an appropriately named variable that will have the value of an array
 
-const entries = []
+const entries = [];
 
-// use the push() method to add the journalEntry objects you defined above to the array
 
-function addEntry() {
-  entries.push(objectsJournalEntry);
-}
+// use the push() method to add the journal entry objects you defined above to the array
 
-function addEntry() {
-  entries.push(notationJournalEntry);
-}
+function addJournalEntry(entryObject) {
+  entries.push(entryObject);
+  console.log("entries", entries);
+};
 
-console.log(entries);
+addJournalEntry(myJournalEntry);
