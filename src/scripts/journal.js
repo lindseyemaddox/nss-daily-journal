ad5158a992cb2592ae3141eb6164bcfef3cbabab
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-24 11:49:33 -0500

    journal entries printing to dom

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,38 +1,41 @@
 
 console.log("hello, darlin'");
 
-// Define the keys and value for a JavaScript object that
-// represents a journal entry about what you learned today
-
-// addJournalEntry = (journalEntry) => {
-//   return `
-//     <div class="journalEntry">
-//       <h2>${journalEntry.date}</h2>
-//       <p>Concepts Covered: ${journalEntry.concepts}</p>
-//       <p>Entry: ${journalEntry.entry}</p>
-//       <p>Mood: ${journalEntry.mood}</p>
-//     </div>
-//   `
-// }
-
-
-
-
 // define an appropriately named variable that will have the value of an array
 
 const entries = [];
 
+let output = document.getElementById("output");
 
 // use the push() method to add the journal entry objects you defined above to the array
 
 function addJournalEntry(entryObject) {
   entries.push(entryObject);
   console.log("entries", entries);
+  buildTheDom();
 };
 
+let buildTheDom = () => {
+  // loop through entries
+  output.innerHTML = "";
+  for (var i = 0; i < entries.length; i++) {
+    let journalEntry = entries[i];
+    output.innerHTML += entryComponent(journalEntry);
+  };
+}
+
+let entryComponent = (journalEntry) => {
+  return `
+    <div class="journalEntry">
+      <h2>${journalEntry.concepts}</h2>
+      <h4>${journalEntry.date}</h4>
+      <p>${journalEntry.entry}</p>
+      <h3>Mood: ${journalEntry.mood}</h3>
+    </div>
+  `
+}
 
 let submit = document.getElementById("submit");
-
 
 function collectEntryItems() {
   const myJournalEntry = {
